0c6f9013b6cf353a31c4c85803e56ad06d0d0455
Ivan Martinez <martinezezequielivan@gmail.com>
2023-10-03 17:00:13 -0300

    Arreglé barcode scanner

--- components/barcode-component.jsx
+++ components/barcode-component.jsx
@@ -4,89 +4,99 @@ import { BarCodeScanner } from "expo-barcode-scanner";
 
 
 export const Barcode = () => {
-    const [scanned, setScanned] = useState(false);
-    const [text, setText] = useState("");
-    const [hasProduct, setHasProduct] = useState(null);
-    const [showResult, setShowResult] = useState(false);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("");
+  const [hasProduct, setHasProduct] = useState(null);
+  const [showResult, setShowResult] = useState(false);
 
-    const handleBarCodeScanned = async ({ type, data }) => {
-        setScanned(true);
-        setText(data);
-        console.log("Type: " + type + "\nData: " + data);
-        try {
-            const itemEncontrado = items.find(item => item.Id == data);
-            if (itemEncontrado) {
-                console.log("Scan succeeded. Data:", items);
-                setHasProduct(itemEncontrado);
-            }
-        } catch (err) {
-            console.error("Error fetching information:", err);
-        }
-    };
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+    try {
+      const items = [{ Id: '77980229', tacc: 'true' }, { Id: '77995681', tacc: 'true' }, { Id: '77969071', tacc: 'false' }, { Id: '7798101201909', tacc: 'false' }];
+      const itemEncontrado = items.find(item => item.Id == data);
+      if (itemEncontrado) {
+        console.log("Scan succeeded. Data:", items);
+        setHasProduct(itemEncontrado);
+      }
 
-    const ResultOverlay = () => {
-        if (validTicket === null || !showResult) return null;
+      setShowResult(true);
+      setTimeout(() => {
+        setScanned(false);
+        setHasProduct(null);
+        setShowResult(false);
+      }, 1000);
+    } catch (err) {
+      console.error("Error fetching information:", err);
+    }
+  };
 
-        return (
-            <View
-                style={[
-                    styles.overlay,
-                    {
-                        backgroundColor: validTicket
-                            ? "rgba(0, 255, 0, 0.7)"
-                            : "rgba(255, 0, 0, 0.7)",
-                    },
-                ]}
-            >
-                <Text style={styles.overlayText}>
-                    {validTicket ? "✓ Valid" : "✕ Invalid"}
-                </Text>
-            </View>
-        );
-    };
+  const ResultOverlay = () => {
+    if (!showResult) return null;
+
+    let backgroundColor, icon, text;
+
+    if (hasProduct) {
+      if (hasProduct.tacc === 'true') {
+        backgroundColor = "rgba(255, 0, 0, 0.7)";
+        icon = "✕";
+        text = "Contiene Gluten";
+      } else {
+        backgroundColor = "rgba(0, 255, 0, 0.7)";
+        icon = "✓";
+        text = "Libre de Gluten";
+      }
+    } else {
+      backgroundColor = "rgba(255, 255, 0, 0.7)";
+      text = "No reconocido";
+    }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.barcodebox}>
-                <BarCodeScanner
-                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                    style={{ height: 500, width: 400 }}
-                />
-                {ResultOverlay()}
-            </View>
-        </View>
+      <View style={[styles.overlay, { backgroundColor }]}>
+        {icon && <Text style={styles.overlayIcon}>{icon}</Text>}
+        <Text style={styles.overlayText}>{text}</Text>
+      </View>
     );
-};
+  };
+
+
+
+
+  return (
+    <View style={styles.barcodebox}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={{ height: '100%', width: '100%' }}
+      />
+      <ResultOverlay />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#000",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        paddingTop: 100,
-    },
-
-    barcodebox: {
-        alignItems: "center",
-        justifyContent: "center",
-        height: 250,
-        width: 800,
-        overflow: "hidden",
-        borderRadius: 20,
-    },
-    overlay: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    overlayText: {
-        fontSize: 50,
-        fontWeight: "bold",
-        color: "#fff",
-    },
-});
+  barcodebox: {
+    height: '100%',
+    width: '100%'
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  overlayIcon: {
+    fontSize: 80,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  overlayText: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+})
