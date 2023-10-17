b06efcc1b2e5ff5f4b0f82664616cf06ac1e52e1
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2023-10-17 17:47:40 -0300

    :sparkles:Fetcheo producto del backend y lo envío al componente Details

--- App.js
+++ App.js
@@ -6,18 +6,10 @@ import AWS from "aws-sdk";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { Details } from "./components/details-component";
 
-/* const { ACCESS_KEY, SECRET_ACCESS_KEY } = Constants.manifest.extra;
-
-AWS.config.update({
-  region: "us-east-1",
-  accessKeyId: ACCESS_KEY,
-  secretAccessKey: SECRET_ACCESS_KEY,
-}); */
 
 export default function App() {
-  
+
   const [hasPermission, setHasPermission] = useState(null);
-  const docClient = new AWS.DynamoDB.DocumentClient();
 
   const askForCameraPermission = () => {
     (async () => {
@@ -30,43 +22,17 @@ export default function App() {
     askForCameraPermission();
   }, []);
 
-  // const fetchTicketInformation = async (ticketId) => {
-  //   return new Promise((resolve, reject) => {
-  //     const params = {
-  //       TableName: "Ticket-zn4tkt5eivea5af5egpjlychcm-dev",
-  //       FilterExpression: "id = :id",
-  //       ExpressionAttributeValues: {
-  //         ":id": ticketId,
-  //       },
-  //     };
-
-  //     docClient.scan(params, (err, items) => {
-  //       if (err) {
-  //         console.error(
-  //           "Unable to scan the table. Error JSON:",
-  //           JSON.stringify(err, null, 2)
-  //         );
-  //         reject(err);
-  //       } else {
-  //         resolve(items);
-  //       }
-  //     });
-  //   });
-  // };
-
-  
-
   return (
     <View style={styles.barcodebox}>
-        <Barcode/>
-        {/* <Details/> */}
-        {/* {ResultOverlay()} */}
-      </View>
+      <Barcode />
+      {/* <Details/> */}
+      {/* {ResultOverlay()} */}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    barcodebox: {
+  barcodebox: {
     alignItems: "center",
     justifyContent: "center",
     height: '100%',
