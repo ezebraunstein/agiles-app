f2182d4fcad44ad289470e85fb930ec570483c7e
ivazquezinama <ivazquezinama@gmail.com>
2023-10-17 20:26:58 -0300

    Agrego LogIn con Cognito

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Pressable, Text } from "react-native";
 import { Barcode } from "./components/barcode-component";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import { Details } from "./components/details-component";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+import config from "./src/aws-exports";
+
+Amplify.configure(config);
+const userSelector = (context) => [context.user]
 
 
-export default function App() {
+const App = () => {
 
   const [hasPermission, setHasPermission] = useState(null);
+  const { user, signOut } = useAuthenticator(userSelector);
 
   const askForCameraPermission = () => {
     (async () => {
@@ -19,12 +25,14 @@ export default function App() {
   useEffect(() => {
     askForCameraPermission();
   }, []);
+  
 
   return (
     <View style={styles.barcodebox}>
+      <Pressable onPress={signOut} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Sign out!</Text>
+      </Pressable>
       <Barcode />
-      {/* <Details/> */}
-      {/* {ResultOverlay()} */}
     </View>
   );
 };
@@ -37,4 +45,14 @@ const styles = StyleSheet.create({
     width: '100%',
     overflow: "hidden",
   },
+  buttonContainer: {
+    alignSelf: 'center',
+    backgroundColor: 'black',
+    paddingHorizontal: 8,
+    top: 100,
+    left: 100,
+  },
+  buttonText: {color: 'white', padding: 16, fontSize: 18},
 });
+
+export default withAuthenticator (App);
